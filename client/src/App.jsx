02381edcf486fde54404e20612030a48cda998e2
Abhinav Sharma <abhinav291sharma@gmail.com>
2024-11-03 17:07:17 +0530

    Updated: App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import React from 'react';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import './App.css';
+import Landing from './pages/landing';
+import UserDashboard from './pages/userDashboard';
+import UserSignup from './pages/userSignup';
+import UserLogin from './pages/userLogin';
+import ProtectedComponent from './components/protectedComponent';
 
 function App() {
-
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landing />}></Route>
+        <Route path='/user/signup' element={<UserSignup />}></Route>
+        <Route path='/user/login' element={<UserLogin />}></Route>
+        <Route path='/user/dashboard' element={<ProtectedComponent><UserDashboard /></ProtectedComponent>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
