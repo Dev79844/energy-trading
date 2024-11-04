859532ae2c41ccb9fe2e55641e9f42de129fc25b
Abhinav Sharma <abhinav291sharma@gmail.com>
2024-11-05 00:46:37 +0530

    Update: Modified marketplace page Added: Sample data for marketplace page

---
+++ client/src/pages/marketPlacePage/marketplace.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import UserNav from "../../components/DashboardNavigation/UserNav";
+import { TextField, FormControl, Button, Typography, Box } from "@mui/material";
+
+const marketplace = () => {
+
+}
+
+export default marketplace;
