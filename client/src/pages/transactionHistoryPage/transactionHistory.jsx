41646a56d86c4623edeafe32ff6be520d7957140
Abhinav Sharma <abhinav291sharma@gmail.com>
2024-11-05 00:49:27 +0530

    Update: Modified transaction history page Added: sample transaction history data

---
+++ client/src/pages/transactionHistoryPage/transactionHistory.jsx
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import { TextField, FormControl, Button, Typography, Box } from "@mui/material";
+
+const transactionHistory = () => {
+
+}
+
+export default transactionHistory;
