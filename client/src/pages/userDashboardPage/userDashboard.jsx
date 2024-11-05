ec6db2d481cc68d8abe4b0487ab6c3804a9c8d1a
Abhinav Sharma <abhinav291sharma@gmail.com>
2024-11-05 09:46:06 +0530

    Update: user dashboard

--- client/src/pages/userDashboardPage/userDashboard.jsx
+++ client/src/pages/userDashboardPage/userDashboard.jsx
@@ -1,8 +1,19 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import UserNav from "../../components/DashboardNavigation/UserNav";
+import { Divider, Typography } from "@mui/material";
 
 export default function UserDashboard() {
+
+    //modify accordingly
+    const userData = useState({
+        emailId: "",
+        unitsSold: 0,
+        unitsPurchased: 0,
+        balance: 0
+    });
+
+    const [amountToAdd, setAmountToAdd] = useState(0);
 
     useEffect(() => {
         //add logic here
@@ -12,7 +23,27 @@ export default function UserDashboard() {
     return (
         <>
             <UserNav />
-            <h1>Hello World!</h1>
+            <Box>
+                <Typography variant="h5">
+                    Your Stats
+                </Typography>
+                <Typography>
+                    Email Id: {userData.emailId}
+                </Typography>
+                <Typography>
+                    Units Sold: {userData.unitsSold}
+                </Typography>
+                <Typography>
+                    Units Purchased: {userData.unitsSold}
+                </Typography>
+                <Typography>
+                    Account Balance: {userData.balance}
+                </Typography>
+
+                <Divider>
+                    {/*Add component*/}
+                </Divider>
+            </Box>
         </>
     );
 }
