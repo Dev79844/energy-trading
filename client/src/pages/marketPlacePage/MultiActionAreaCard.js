1a53fd206428dd3ea9f2c0a10341d3167e08441a
Abhinav Sharma <abhinav291sharma@gmail.com>
2024-11-05 20:36:59 +0530

    Added: new card component

---
+++ client/src/pages/marketPlacePage/MultiActionAreaCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import CardActions from '@mui/material/CardActions';
+
+export default function MultiActionAreaCard({ item, onBuy }) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Seller ID: {item.email}
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Units Available: {item.numberOfUnits}
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Amount Quoted: ${item.amountQuoted}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" color="primary" onClick={onBuy}>
+          Buy
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
